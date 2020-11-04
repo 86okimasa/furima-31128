bcd3fc65566b8e7436d52f5eb19358e863578403
86okimasa <tc.okimoto@icloud.com>
2020-11-04 22:11:24 +0900

    ActiveHash導入及びカテゴリー選択の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,27 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 
   private
 
-  def message_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:image, :category_id).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,20 @@
 class Item < ApplicationRecord
 
-belongs_to :user
-has_one_attached :image
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping
+  belongs_to :prefecture
+  belongs_to :dispatch
+
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_id
+    validates :prefecture_id
+    validates :dispatch_id
+  end
 end
