d96ff08e1224f0fe69782f44fe219dc349146225
86okimasa <tc.okimoto@icloud.com>
2020-11-04 16:43:25 +0900

    Active Storage

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,12 @@ class ItemsController < ApplicationController
 
   def new
   end
+
+
+  private
+
+  def message_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
+
 end
