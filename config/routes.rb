dee99d883ad113d26cd39053aa19c1347c9a86d5
86okimasa <tc.okimoto@icloud.com>
2020-11-10 21:46:26 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: "items#index"
   devise_for :users
-  resources :items, only: [:new, :create, :index, :show, :edit, :destroy]
+  resources :items, only: [:new, :create, :index, :show, :edit, :destroy] do
+    resources :purchases, only: [:index, :create]
+  end
 end
