4516a76b0a1de38a01e4eff565d6920019d0e39b
86okimasa <tc.okimoto@icloud.com>
2020-11-11 17:13:27 +0900

    コントローラーの修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,18 +1,20 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+
   def index
     @item = Item.find(params[:item_id])
+    @purchases = Purchase.all
+    if @item.user_id == current_user.id || @purchases.exists?(item_id: @item.id)
+      redirect_to root_path
+    end
+
     @item_purchase = ItemPurchase.new
   end
 
   def create
     @item_purchase = ItemPurchase.new(purchase_params)
       if @item_purchase.valid?
-      Payjp.api_key = "sk_test_17efb7ed25673a19bd80a8d2"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
-      Payjp::Charge.create(
-        amount: purchase_params[:price],  # 商品の値段
-        card: purchase_params[:token],    # カードトークン
-        currency: 'jpy'                 # 通貨の種類（日本円）
-      )
+        pay_item
         @item_purchase.save
         redirect_to root_path
       else
@@ -26,6 +28,15 @@ class PurchasesController < ApplicationController
     def purchase_params
       @item = Item.find(params[:item_id])
       params.require(:item_purchase).permit(:postal_code, :prefecture_id, :municipalities, :address, :building, :phone_number).merge(token: params[:token], price: @item.price, item_id: @item.id, user_id: current_user.id)
+    end
+
+    def pay_item
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: purchase_params[:price],  # 商品の値段
+        card: purchase_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
     end
 
 end
