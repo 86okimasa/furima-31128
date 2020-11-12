58c9f76bfdea03037d229ddc17c9ab50f266459a
86okimasa <tc.okimoto@icloud.com>
2020-11-12 11:36:41 +0900

    コントローラーの修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,8 +1,8 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: [:index]
+  before_action :item_find, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     if @item.user_id == current_user.id || @item.purchase != nil
       redirect_to root_path
     end
@@ -16,15 +16,14 @@ class PurchasesController < ApplicationController
         @item_purchase.save
         redirect_to root_path
       else
-        @item = Item.find(params[:item_id])
         render :index
       end
   end
 
 
   private
+
     def purchase_params
-      @item = Item.find(params[:item_id])
       params.require(:item_purchase).permit(:postal_code, :prefecture_id, :municipalities, :address, :building, :phone_number).merge(token: params[:token], price: @item.price, item_id: @item.id, user_id: current_user.id)
     end
 
@@ -35,6 +34,10 @@ class PurchasesController < ApplicationController
         card: purchase_params[:token],    # カードトークン
         currency: 'jpy'                 # 通貨の種類（日本円）
       )
+    end
+
+    def item_find
+      @item = Item.find(params[:item_id])
     end
 
 end
