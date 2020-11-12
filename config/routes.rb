b55bfc97bf20f5636e20931e7cdeffb733690bf7
86okimasa <tc.okimoto@icloud.com>
2020-11-12 17:00:45 +0900

    商品編集修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "items#index"
   devise_for :users
-  resources :items, only: [:new, :create, :index, :show, :edit, :destroy] do
+  resources :items, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
     resources :purchases, only: [:index, :create]
   end
 end
